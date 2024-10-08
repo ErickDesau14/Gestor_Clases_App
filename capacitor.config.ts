79171852cf7a0bf14ddef592b6b7c76f545eab33
ErickDesau14 <erick_1322134389@uptecamac.edu.mx>
2024-10-07 22:52:45 -0600

    Configuración inicial del proyecto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: '07_gestor_clases',
+  appName: 'Gestor de clases',
   webDir: 'www'
 };
 
