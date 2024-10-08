c10fa8fce3222e99a3d48adfdfb572b7335f80af
ErickDesau14 <erick_1322134389@uptecamac.edu.mx>
2024-10-08 11:25:23 -0600

    Recuperando base de datos

--- service/sqlite-manager.service.ts
+++ service/sqlite-manager.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Device} from "@capacitor/device";
-import {CapacitorSQLite} from "@capacitor-community/sqlite";
+import {CapacitorSQLite, JsonSQLite} from "@capacitor-community/sqlite";
 import {AlertController} from "@ionic/angular";
 import {Preferences} from "@capacitor/preferences";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,16 @@ import {Preferences} from "@capacitor/preferences";
 export class SqliteManagerService {
 
   public isWeb: boolean;
-
+  private dbName: string;
   private DB_SETUP_KEY = 'first_db_setup';
+  private DB_NAME_KEY = 'db_name';
 
   constructor(
-    private alertController: AlertController
+    private alertController: AlertController,
+    private httpClient: HttpClient
   ) {
     this.isWeb = false;
+    this.dbName = '';
   }
 
   async init() {
@@ -44,12 +48,44 @@ export class SqliteManagerService {
       const  dbSetupDone = await Preferences.get({key: this.DB_SETUP_KEY});
 
       if (!dbSetupDone.value) {
-
+        this.downloadDatabase();
       } else {
+
+        const dbName = await this.getDBName();
+        await CapacitorSQLite.createConnection({ database: dbName });
+        await CapacitorSQLite.open({ database: dbName });
 
       }
     }
 
+  }
+
+  downloadDatabase() {
+
+    this.httpClient.get('assets/db/db.json').subscribe(async (jsonExport: JsonSQLite) => {
+      const  jsonstring = JSON.stringify(jsonExport);
+      const isValid = await CapacitorSQLite.isJsonValid({ jsonstring });
+
+      if (isValid.result) {
+        this.dbName = jsonExport.database;
+        await CapacitorSQLite.importFromJson({ jsonstring });
+        await CapacitorSQLite.createConnection({ database: this.dbName });
+        await CapacitorSQLite.open({ database: this.dbName });
+
+        await Preferences.set({key: this.DB_SETUP_KEY, value: '1'});
+        await Preferences.set({key: this.DB_NAME_KEY, value: this.dbName});
+
+      }
+    })
+
+  }
+
+  async getDBName() {
+    if(!this.dbName) {
+      const dbName = await Preferences.get({key: this.DB_NAME_KEY});
+      this.dbName = dbName.value;
+    }
+    return this.dbName;
   }
 
 }
